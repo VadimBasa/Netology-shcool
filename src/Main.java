0f9989c44d7d5a8304a73a6dd64322e30b16c99a
Vadim Basalaev <basalaev.vad@yandex.ru>
2022-07-22 09:40:13 +0400

    Исправлен README

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+    SalesManager manager = new SalesManager(new int[]{1, 5, 4});
+        System.out.println(manager.max());
+    }
 }
