8cfd7e5c026176c4b69eb8ac989395e6ec1cd387
Vadim Basalaev <basalaev.vad@yandex.ru>
2022-07-27 23:24:05 +0400

    stat

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-    SalesManager manager = new SalesManager(new int[]{1, 5, 4});
-        System.out.println(manager.max());
+        SalesManager manager = new SalesManager(new int[]{1, 5, 4});
+        System.out.println(manager.average());
     }
 }
